35dec44ce8b30e37674d9234dcd7b2989b68fe35
Andrey <88499258+Andrey228v@users.noreply.github.com>
2026-03-19 22:52:40 +0300

    boot

--- Assets/_Scripts/EnteryPoints/BootEntryPoint.cs
+++ Assets/_Scripts/EnteryPoints/BootEntryPoint.cs
@@ -2,17 +2,18 @@
 using Assets._Scripts.Utilites;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using VContainer.Unity;
 
 namespace Assets._Scripts.EnteryPoints
 {
     public class BootEntryPoint : IInitializable
     {
-        private AsyncSceneLoading _sceneLoading;
+        //private AsyncSceneLoading _sceneLoading;
 
-        public BootEntryPoint(AsyncSceneLoading sceneLoading)
+        public BootEntryPoint()
         {
-            _sceneLoading = sceneLoading;
+            //_sceneLoading = sceneLoading;
         }
 
         public async void Initialize()
@@ -20,7 +21,9 @@ namespace Assets._Scripts.EnteryPoints
             Application.targetFrameRate = 60;
             Screen.sleepTimeout = SleepTimeout.NeverSleep;
             DOTween.SetTweensCapacity(5000, 100);
-            await _sceneLoading.LoadScene(Scenes.MENU);
+            //await _sceneLoading.LoadScene(Scenes.MENU);
+            //await SceneManager.LoadSceneAsync("Boot", LoadSceneMode.Single);
+
         }
     }
 }

b6d6fa43ab4d09cc52804d9f97cc525b021fd357
Andrey <gysev003@mail.ru>
2026-04-06 16:29:00 +0300

    Platforms v1

---
+++ Assets/_Scripts/Platforms/MovingPlatform.cs
@@ -0,0 +1,42 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Assets._Scripts.Platforms
+{
+    class MovingPlatform : MonoBehaviour
+    {
+        [SerializeField] private float _moveDistance = 3f;
+        [SerializeField] private float _duration = 2f;
+        [SerializeField] private Axis _moveAxis = Axis.X;
+
+
+
+        private Tween _tweener;
+        private Vector3 _startPosition;
+        private Vector3 _endPosition;
+
+        private enum Axis { X, Y, Z }
+
+        private void Start()
+        {
+            _startPosition = transform.position;
+            _endPosition = CalculateEndPosition();
+        }
+
+        private Vector3 CalculateEndPosition()
+        {
+            Vector3 endPos = _startPosition;
+
+            switch (_moveAxis)
+            {
+                case Axis.X: endPos.x += _moveDistance; break;
+                case Axis.Y: endPos.y += _moveDistance; break;
+                case Axis.Z: endPos.z += _moveDistance; break;
+            }
+
+            return endPos;
+        }
+
+
+    }
+}

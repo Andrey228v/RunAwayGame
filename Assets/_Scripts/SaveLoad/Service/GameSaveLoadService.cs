e050682c6dcb45ae53e84904f8967ebce68cf37f
Andrey <gysev003@mail.ru>
2026-04-22 13:47:17 +0300

    SL ref1

---
+++ Assets/_Scripts/SaveLoad/Service/GameSaveLoadService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assets._Scripts.SaveLoad.Service
+{
+    public class GameSaveLoadService
+    {
+        //private List<>
+
+        public void SaveGame()
+        {
+
+        }
+
+        public void LoadGame() 
+        {
+
+        }
+
+
+    }
+}

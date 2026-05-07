763e5e1b5d68e39a2d9453df7cc7e68a1fbd111b
Andrey <gysev003@mail.ru>
2026-05-07 16:33:46 +0300

    NUnit tests v1

---
+++ Assets/Tests/Test1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class Test1
+{
+    [Test]
+    public void AddPoints_IncreasesProgress()
+    {
+
+    }
+}

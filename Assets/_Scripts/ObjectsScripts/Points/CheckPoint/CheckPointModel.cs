85665a68d635cfdef3916b2de81b123424b40189
Andrey <88499258+Andrey228v@users.noreply.github.com>
2026-07-06 23:06:05 +0300

    Bobit ID ... . 666

--- Assets/_Scripts/ObjectsScripts/Points/CheckPoint/CheckPointModel.cs
+++ Assets/_Scripts/ObjectsScripts/Points/CheckPoint/CheckPointModel.cs
@@ -1,31 +1,28 @@
 ﻿using Assets.Scripts.SaveLoad.Data;
 using System;
+using UnityEngine;
 
 namespace Assets._Scripts.ObjectsScripts.Points.CheckPoint
 {
     public class CheckPointModel
     {
-        //private bool _isActivate;
-        //private readonly string _id;
         private CheckPointData _data;
 
         public event Action<string, bool> OnObjectStatusChange;
         public event Action<int> OnTakeValue;
         public event Action OnTake;
 
+        public CheckPointData Data => _data;
+
         public bool IsActivate => _data.IsActivated;
 
         public CheckPointModel(CheckPointData data)
         {
             _data = data;
-            //_id = data.Id;
-            //_isActivate = data.IsActivated;
         }
 
         public void SetActivateStatus(bool isActivate)
         {
-            if (_data.IsActivated == isActivate) return;
-
             _data.IsActivated = isActivate;
 
             OnObjectStatusChange?.Invoke(_data.Id, _data.IsActivated);
